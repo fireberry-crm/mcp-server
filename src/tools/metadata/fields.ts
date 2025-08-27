b708359b27609ed91ebd26ecfc55aeabd9099a6c
Aviv Nakar <62398678+avivnakar@users.noreply.github.com>
2025-08-27 15:15:09 +0300

    feat: object type validation

--- src/tools/metadata/fields.ts
+++ src/tools/metadata/fields.ts
@@ -2,7 +2,7 @@ import { z } from 'zod';
 import { FieldTypes, type FieldTypeName } from '../../constants.js';
 
 export const metadataFieldsToolInputSchema = z.object({
-    objectType: z.int().describe('The object type to get metadata for'),
+    objectType: z.int().min(1).describe('The object type to get metadata for'),
 });
 
 const metadataFieldBaseSchema = z.object({
