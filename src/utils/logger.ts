5fd6029bdefb2cfc0c16e8b0a926017d83e75b7f
Aviv Nakar <62398678+avivnakar@users.noreply.github.com>
2025-08-27 15:14:49 +0300

    feat: switch logger methods according to transport type

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -5,6 +5,28 @@ import { env } from '../env.js';
 
 type LogLevel = 'debug' | 'info' | 'warn' | 'error';
 
+const levels = {
+    debug: 0,
+    info: 1,
+    warn: 2,
+    error: 3,
+} as const satisfies Record<LogLevel, number>;
+
+const logMethods: Record<LogLevel, (message: unknown, ...args: unknown[]) => void> =
+    env.TRANSPORT === 'stdio'
+        ? {
+              debug: console.error,
+              info: console.error,
+              warn: console.error,
+              error: console.error,
+          }
+        : {
+              debug: console.debug,
+              info: console.info,
+              warn: console.warn,
+              error: console.error,
+          };
+
 class Logger {
     private level: LogLevel;
 
@@ -13,36 +35,32 @@ class Logger {
     }
 
     private shouldLog(level: LogLevel): boolean {
-        const levels = {
-            debug: 0,
-            info: 1,
-            warn: 2,
-            error: 3,
-        } as const satisfies Record<LogLevel, number>;
         return levels[level] >= levels[this.level];
     }
-
+    get date(): string {
+        return new Date().toISOString().split('T').join(' ').split('Z').join('');
+    }
     debug(message: string, ...args: unknown[]): void {
         if (this.shouldLog('debug')) {
-            console.warn(`[DEBUG] ${message}`, ...args);
+            logMethods.debug(`${this.date} [DEBUG] ${message}`, ...args);
         }
     }
 
     info(message: string, ...args: unknown[]): void {
         if (this.shouldLog('info')) {
-            console.warn(`[INFO] ${message}`, ...args);
+            logMethods.info(`${this.date} [INFO] ${message}`, ...args);
         }
     }
 
     warn(message: string, ...args: unknown[]): void {
         if (this.shouldLog('warn')) {
-            console.warn(`[WARN] ${message}`, ...args);
+            logMethods.warn(`${this.date} [WARN] ${message}`, ...args);
         }
     }
 
     error(message: string, ...args: unknown[]): void {
         if (this.shouldLog('error')) {
-            console.error(`[ERROR] ${message}`, ...args);
+            logMethods.error(`${this.date} [ERROR] ${message}`, ...args);
         }
     }
 }
