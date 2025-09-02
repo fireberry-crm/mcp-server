ac4ee7c9db4d4744dd7c65c261b0fe84a0416326
Aviv Nakar <62398678+avivnakar@users.noreply.github.com>
2025-09-02 15:15:55 +0300

    fix: remove is main check

--- src/cli.ts
+++ src/cli.ts
@@ -1,11 +1,7 @@
 import { main } from './stdio.js';
 import { logger } from './utils/index.js';
 
-const isMain = import.meta.url.includes(process.argv[1] ?? import.meta.url + '#not main');
-
-if (isMain) {
-    main().catch((error: unknown) => {
-        logger.error('Server error:', error);
-        process.exit(1);
-    });
-}
+main().catch((error: unknown) => {
+    logger.error('Server error:', error);
+    process.exit(1);
+});
